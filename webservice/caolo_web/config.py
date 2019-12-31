69a60c5b6f6216ebe3f4039a63640920b72f8d36
Daniel Kiss <littlesnorrboy@gmail.com>
2019-12-31 14:54:43 +0100

    Use the os.getenv optional params

--- webservice/caolo_web/config.py
+++ webservice/caolo_web/config.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.getenv("FLASK_SECRET_KEY") or "asdasdasdasdas"
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "asdasdasdasdas")
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL") or "postgres://postgres:almafa1@localhost/caolo"
+        "DATABASE_URL", "postgres://postgres:almafa1@localhost/caolo")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GOOGLE_OAUTH_CLIENT_ID = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
     GOOGLE_OAUTH_CLIENT_SECRET = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET")

d79f5c207d4e309640cea47e08ee744c1c7c7fe8
Daniel Kiss <littlesnorrboy@gmail.com>
2020-04-13 14:18:07 +0200

    Parametrize morton table benchmarks

--- simulation/benches/morton_table.rs
+++ simulation/benches/morton_table.rs
@@ -2,113 +2,107 @@ use caolo_sim::model::components::EntityComponent;
 use caolo_sim::model::geometry::{Circle, Point};
 use caolo_sim::model::EntityId;
 use caolo_sim::tables::{MortonTable, PositionTable};
-use criterion::{criterion_group, Criterion};
+use criterion::{criterion_group, BenchmarkId, Criterion};
 use rand::Rng;
 use rand::RngCore;
 
-fn contains_rand_at_2pow16(c: &mut Criterion) {
-    c.bench_function("morton contains rand 2^16", move |b| {
-        let mut rng = rand::thread_rng();
+fn contains_rand(c: &mut Criterion) {
+    let mut group = c.benchmark_group("contains_rand");
+    for size in 8..16 {
+        let size = 1 << size;
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, move |b, &size| {
+            let mut rng = rand::thread_rng();
 
-        let table = MortonTable::from_iterator((0..(1 << 16)).map(|i| {
-            let p = Point {
-                x: rng.gen_range(0, 8000),
-                y: rng.gen_range(0, 8000),
-            };
-            (p, i)
-        }))
-        .unwrap();
-        b.iter(|| {
-            let p = Point {
-                x: rng.gen_range(0, 8000),
-                y: rng.gen_range(0, 8000),
-            };
-            table.contains_key(&p)
-        })
-    });
+            let table = MortonTable::from_iterator((0..size).map(|i| {
+                let p = Point {
+                    x: rng.gen_range(0, 8000),
+                    y: rng.gen_range(0, 8000),
+                };
+                (p, i)
+            }))
+            .unwrap();
+
+            b.iter(|| {
+                let p = Point {
+                    x: rng.gen_range(0, 8000),
+                    y: rng.gen_range(0, 8000),
+                };
+                table.contains_key(&p)
+            })
+        });
+    }
+    group.finish();
 }
 
 fn get_entities_in_range_sparse(c: &mut Criterion) {
-    c.bench_function("get_entities_in_range sparse", |b| {
-        let mut rng = rand::thread_rng();
+    let mut group = c.benchmark_group("get_entities_in_range sparse");
+    for size in 8..16 {
+        let size = 1 << size;
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+            let mut rng = rand::thread_rng();
 
-        let table = MortonTable::from_iterator((0..1 << 12).map(|_| {
-            let p = Point {
-                x: rng.gen_range(0, 3900 * 2),
-                y: rng.gen_range(0, 3900 * 2),
-            };
-            (p, EntityComponent(EntityId(rng.gen())))
-        }))
-        .unwrap();
+            let table = MortonTable::from_iterator((0..size).map(|_| {
+                let p = Point {
+                    x: rng.gen_range(0, 3900 * 2),
+                    y: rng.gen_range(0, 3900 * 2),
+                };
+                (p, EntityComponent(EntityId(rng.gen())))
+            }))
+            .unwrap();
 
-        let radius = 512;
-        b.iter(|| {
-            let table = &table;
-            let p = Point {
-                x: rng.gen_range(0, 3900 * 2),
-                y: rng.gen_range(0, 3900 * 2),
-            };
-            table.get_entities_in_range(&Circle { center: p, radius })
+            let radius = 512;
+            b.iter(|| {
+                let table = &table;
+                let p = Point {
+                    x: rng.gen_range(0, 3900 * 2),
+                    y: rng.gen_range(0, 3900 * 2),
+                };
+                table.get_entities_in_range(&Circle { center: p, radius })
+            });
         });
-    });
+    }
+    group.finish();
 }
 
 fn get_entities_in_range_dense(c: &mut Criterion) {
-    c.bench_function("get_entities_in_range dense", |b| {
-        let mut rng = rand::thread_rng();
+    let mut group = c.benchmark_group("get_entities_in_range dense");
+    for size in 8..16 {
+        let size = 1 << size;
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+            let mut rng = rand::thread_rng();
 
-        let table = MortonTable::from_iterator((0..1 << 12).map(|_| {
-            let p = Point {
-                x: rng.gen_range(0, 200 * 2),
-                y: rng.gen_range(0, 200 * 2),
-            };
-            (p, EntityComponent(EntityId(rng.gen())))
-        }))
-        .unwrap();
+            let table = MortonTable::from_iterator((0..size).map(|_| {
+                let p = Point {
+                    x: rng.gen_range(0, 200 * 2),
+                    y: rng.gen_range(0, 200 * 2),
+                };
+                (p, EntityComponent(EntityId(rng.gen())))
+            }))
+            .unwrap();
 
-        let radius = 50;
-        b.iter(|| {
-            let table = &table;
-            let p = Point {
-                x: rng.gen_range(0, 200 * 2),
-                y: rng.gen_range(0, 200 * 2),
-            };
-            table.get_entities_in_range(&Circle { center: p, radius })
+            let radius = 50;
+            b.iter(|| {
+                let table = &table;
+                let p = Point {
+                    x: rng.gen_range(0, 200 * 2),
+                    y: rng.gen_range(0, 200 * 2),
+                };
+                table.get_entities_in_range(&Circle { center: p, radius })
+            });
         });
-    });
+    }
+    group.finish();
 }
 
 fn make_morton_table(c: &mut Criterion) {
-    c.bench_function("make morton table", |b| {
-        let mut rng = rand::thread_rng();
+    let mut group = c.benchmark_group("make_morton_table dense");
+    for size in 8..16 {
+        let size = 1 << size;
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+            let mut rng = rand::thread_rng();
 
-        b.iter(|| {
-            let table = MortonTable::from_iterator((0..(1 << 15)).map(|_| {
-                (
-                    Point {
-                        x: rng.gen_range(0, 3900 * 2),
-                        y: rng.gen_range(0, 3900 * 2),
-                    },
-                    rng.next_u32(),
-                )
-            }))
-            .unwrap();
-            table
-        });
-    });
-}
-
-fn rebuild_morton_table(c: &mut Criterion) {
-    c.bench_function("rebuild_morton_table", |b| {
-        let mut rng = rand::thread_rng();
-
-        let mut table = MortonTable::with_capacity(1 << 15);
-
-        b.iter(|| {
-            table.clear();
-
-            table
-                .extend((0..(1 << 15)).map(|_| {
+            b.iter(|| {
+                let table = MortonTable::from_iterator((0..size).map(|_| {
                     (
                         Point {
                             x: rng.gen_range(0, 3900 * 2),
@@ -118,41 +112,78 @@ fn rebuild_morton_table(c: &mut Criterion) {
                     )
                 }))
                 .unwrap();
+                table
+            });
         });
-    });
+    }
+    group.finish();
 }
 
-fn get_by_id_rand_2pow16(c: &mut Criterion) {
-    c.bench_function("get_by_id random in 2^16", |b| {
-        let mut rng = rand::thread_rng();
+fn rebuild_morton_table(c: &mut Criterion) {
+    let mut group = c.benchmark_group("make_morton_table dense");
+    for size in 8..16 {
+        let size = 1 << size;
 
-        let len = 1 << 16;
-        let table = MortonTable::from_iterator((0..len).map(|_| {
-            let pos = Point {
-                x: rng.gen_range(0, 3900 * 2),
-                y: rng.gen_range(0, 3900 * 2),
-            };
-            (pos, rng.next_u32())
-        }))
-        .unwrap();
-
-        b.iter(|| {
-            let pos = Point {
-                x: rng.gen_range(0, 3900 * 2),
-                y: rng.gen_range(0, 3900 * 2),
-            };
-            table.get_by_id(&pos)
-        });
-    });
-}
-
-fn get_by_id_in_table_rand_2pow16(c: &mut Criterion) {
-    c.bench_function(
-        "get_by_id 2^16 elements, all queried elements are in the table",
-        |b| {
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
             let mut rng = rand::thread_rng();
 
-            let len = 1 << 16;
+            let mut table = MortonTable::with_capacity(size);
+
+            b.iter(|| {
+                table.clear();
+
+                table
+                    .extend((0..size).map(|_| {
+                        (
+                            Point {
+                                x: rng.gen_range(0, 3900 * 2),
+                                y: rng.gen_range(0, 3900 * 2),
+                            },
+                            rng.next_u32(),
+                        )
+                    }))
+                    .unwrap();
+            });
+        });
+    }
+    group.finish();
+}
+
+fn get_by_id_rand(c: &mut Criterion) {
+    let mut group = c.benchmark_group("get_by_id random");
+    for size in 8..16 {
+        let size = 1 << size;
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &len| {
+            let mut rng = rand::thread_rng();
+
+            let table = MortonTable::from_iterator((0..len).map(|_| {
+                let pos = Point {
+                    x: rng.gen_range(0, 3900 * 2),
+                    y: rng.gen_range(0, 3900 * 2),
+                };
+                (pos, rng.next_u32())
+            }))
+            .unwrap();
+
+            b.iter(|| {
+                let pos = Point {
+                    x: rng.gen_range(0, 3900 * 2),
+                    y: rng.gen_range(0, 3900 * 2),
+                };
+                table.get_by_id(&pos)
+            });
+        });
+    }
+    group.finish();
+}
+
+fn get_by_id_in_table_rand(c: &mut Criterion) {
+    let mut group = c.benchmark_group("get_by_id, all queried elements are in the table");
+    for size in 8..16 {
+        let size = 1 << size;
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &len| {
+            let mut rng = rand::thread_rng();
+
             let mut points = Vec::with_capacity(len);
             let table = MortonTable::from_iterator((0..len).map(|_| {
                 let pos = Point {
@@ -169,42 +200,48 @@ fn get_by_id_in_table_rand_2pow16(c: &mut Criterion) {
                 let pos = &points[i];
                 table.get_by_id(pos)
             });
-        },
-    );
+        });
+    }
+    group.finish();
 }
 
 fn random_insert(c: &mut Criterion) {
-    c.bench_function("random_insert", |b| {
-        let mut rng = rand::thread_rng();
-        let mut table = MortonTable::<Point, usize>::new();
+    let mut group = c.benchmark_group("random_insert");
+    for size in 8..16 {
+        let size = 1 << size;
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+            let mut rng = rand::thread_rng();
+            let mut table = MortonTable::<Point, usize>::new();
 
-        for _ in 0..10_000 {
-            let x = rng.gen_range(0, 29000);
-            let y = rng.gen_range(0, 29000);
-            let p = Point::new(x, y);
+            for _ in 0..size {
+                let x = rng.gen_range(0, 29000);
+                let y = rng.gen_range(0, 29000);
+                let p = Point::new(x, y);
 
-            table.insert(p, 420);
-        }
+                table.insert(p, 420);
+            }
 
-        b.iter(|| {
-            let x = rng.gen_range(0, 29000);
-            let y = rng.gen_range(0, 29000);
-            let p = Point::new(x, y);
+            b.iter(|| {
+                let x = rng.gen_range(0, 29000);
+                let y = rng.gen_range(0, 29000);
+                let p = Point::new(x, y);
 
-            table.insert(p, 420)
+                table.insert(p, 420)
+            });
         });
-    });
+    }
+    group.finish();
 }
 
 criterion_group!(
     morton_benches,
-    contains_rand_at_2pow16,
+    contains_rand,
     get_entities_in_range_sparse,
     get_entities_in_range_dense,
     make_morton_table,
     random_insert,
     rebuild_morton_table,
-    get_by_id_in_table_rand_2pow16,
-    get_by_id_rand_2pow16,
+    get_by_id_in_table_rand,
+    get_by_id_rand,
     rebuild_morton_table,
 );

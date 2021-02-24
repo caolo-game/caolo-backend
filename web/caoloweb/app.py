b9b615479b351f2716a383fe791ff3fd3af03fcf
Daniel Kiss <littlesnorrboy@gmail.com>
2021-02-24 12:54:07 +0100

    Init handlers/

--- web/caoloweb/app.py
+++ web/caoloweb/app.py
@@ -8,6 +8,8 @@ import os
 
 from .api_schema import RoomObjects, Axial, make_room_id, parse_room_id
 
+from .handler import world, scripting, admin
+
 app = FastAPI()
 
 app.add_middleware(
@@ -19,17 +21,23 @@ app.add_middleware(
 )
 
 DB_STR = os.getenv("DATABASE_URL", "postgres://postgres:admin@localhost:5432/caolo")
+DB_POOL = None
+
+
+async def db_pool():
+    global DB_POOL
+    if not DB_POOL:
+        DB_POOL = await asyncpg.create_pool(DB_STR)
+    return DB_POOL
 
 
 @app.middleware("http")
 async def db_session(req, call_next):
     resp = Response(status_code=500)
-    dbconn = await asyncpg.connect(DB_STR)
-    try:
-        req.state.db = dbconn
+    pool = await db_pool()
+    async with pool.acquire() as con:
+        req.state.db = con
         resp = await call_next(req)
-    finally:
-        await req.state.db.close()
     return resp
 
 
@@ -38,82 +46,6 @@ async def health():
     return Response(status_code=204)
 
 
-@app.get("/terrain", response_model=List[Tuple[Axial, str]])
-async def terrain(
-    req: Request, q: str = Query(None, max_length=5), r: str = Query(None, max_length=5)
-):
-    room_id = make_room_id(q, r)
-
-    res_encoded = await req.state.db.fetchval(
-        """
-        SELECT t.payload->'terrain'->$1 AS room
-        FROM public.world_output t
-        ORDER BY t.created DESC
-        """,
-        room_id,
-    )
-
-    # returned data is already json encoded string
-    # TODO: just write the already encoded response...
-    if not res_encoded:
-        return []
-    return json.loads(res_encoded)
-
-
-@app.get("/rooms", response_model=List[Dict])
-async def rooms(req: Request):
-    res = await req.state.db.fetchrow(
-        """
-        SELECT t.payload->'rooms' AS rooms
-        FROM public.world_output t
-        ORDER BY t.created DESC
-        """
-    )
-
-    assert res, "No results returned"
-
-    res_encoded = res["rooms"]
-    data: Dict[str, Dict] = json.loads(res_encoded)
-    # keys are 'q;r', so split them and insert them into a 'pos' object, then put the rest of the values next to it
-    return (
-        {"pos": room_id, **v}
-        for room_id, v in ((parse_room_id(k), v) for k, v in data.items())
-    )
-
-
-@app.get("/room-objects", response_model=RoomObjects)
-async def room_objects(
-    req: Request,
-    q: str = Query(None, max_length=5),
-    r: str = Query(None, max_length=5),
-):
-    """
-    return a list of each type of entity in the given room
-    """
-
-    room_id = make_room_id(q, r)
-
-    # Turns out that loading the entire game state into memory,
-    # parsing the json and filtering it is about 3x faster than doing this filtering in Postgres
-    res = await req.state.db.fetchrow(
-        """
-        SELECT
-            t.payload as payload
-            , t.world_time as time
-        FROM public.world_output t
-        ORDER BY t.created DESC
-        """,
-    )
-    payload = RoomObjects()
-
-    try:
-        pl = json.loads(res["payload"])
-        payload.time = res["time"]
-        payload.payload = {
-            "bots": pl["bots"][room_id],
-            "structures": pl["structures"][room_id],
-            "resources": pl["resources"][room_id],
-        }
-    except KeyError:
-        pass
-    return payload
+app.include_router(world.router)
+app.include_router(scripting.router)
+app.include_router(admin.router)
---
+++ web/caoloweb/handler/world.py
@@ -0,0 +1,123 @@
+from typing import Dict, List, Tuple
+from fastapi import APIRouter, Response, Query, Request
+from fastapi.middleware.cors import CORSMiddleware
+import asyncpg
+import json
+import os
+
+from ..api_schema import RoomObjects, Axial, make_room_id, parse_room_id
+
+
+router = APIRouter(prefix="/world")
+
+
+@router.get("/terrain", response_model=List[Tuple[Axial, str]])
+async def terrain(
+    req: Request,
+    q: str = Query(None, max_length=5),
+    r: str = Query(None, max_length=5),
+):
+    room_id = make_room_id(q, r)
+
+    res_encoded = await req.state.db.fetchval(
+        """
+        SELECT t.payload->'terrain'->$1 AS room
+        FROM public.world_output t
+        ORDER BY t.created DESC
+        """,
+        room_id,
+    )
+
+    # returned data is already json encoded string
+    # TODO: just write the already encoded response...
+    if not res_encoded:
+        return []
+    return json.loads(res_encoded)
+
+
+@router.get("/rooms", response_model=List[Dict])
+async def rooms(req: Request):
+    res = await req.state.db.fetchrow(
+        """
+        SELECT t.payload->'rooms' AS rooms
+        FROM public.world_output t
+        ORDER BY t.created DESC
+        """
+    )
+
+    assert res, "No results returned"
+
+    res_encoded = res["rooms"]
+    data: Dict[str, Dict] = json.loads(res_encoded)
+    # keys are 'q;r', so split them and insert them into a 'pos' object, then put the rest of the values next to it
+    return (
+        {"pos": room_id, **v}
+        for room_id, v in ((parse_room_id(k), v) for k, v in data.items())
+    )
+
+
+@router.get("/room-objects", response_model=RoomObjects)
+async def room_objects(
+    req: Request,
+    q: str = Query(None, max_length=5),
+    r: str = Query(None, max_length=5),
+):
+    """
+    return a list of each type of entity in the given room
+    """
+
+    room_id = make_room_id(q, r)
+
+    # Turns out that loading the entire game state into memory,
+    # parsing the json and filtering it is about 3x faster than doing this filtering in Postgres
+    res = await req.state.db.fetchrow(
+        """
+        SELECT
+            t.payload as payload
+            , t.world_time as time
+        FROM public.world_output t
+        ORDER BY t.created DESC
+        """,
+    )
+    payload = RoomObjects()
+    if not res:
+        return payload
+
+    try:
+        pl = json.loads(res["payload"])
+        payload.time = res["time"]
+        payload.payload = {
+            "bots": pl["bots"][room_id],
+            "structures": pl["structures"][room_id],
+            "resources": pl["resources"][room_id],
+        }
+    except KeyError:
+        pass
+    return payload
+
+
+@router.get("/diagnostics", response_model=Dict)
+async def diagnostics(req: Request):
+    """
+    returns internal engine diagnostics
+    """
+
+    row = await req.state.db.fetchrow(
+        """
+        SELECT
+            t.payload->'diagnostics' as pl
+            , t.world_time as time
+        FROM public.world_output t
+        ORDER BY t.created DESC
+        """,
+    )
+
+    if not row:
+        return None
+    pl = None
+    try:
+        pl = json.loads(row["pl"])
+        pl["tick"] = row.get("time", -1)
+    except KeyError:
+        pass
+    return pl

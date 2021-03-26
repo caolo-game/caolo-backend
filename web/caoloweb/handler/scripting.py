3d5e0c4764f1be6880222920404d622facaafd7a
Daniel Kiss <littlesnorrboy@gmail.com>
2021-03-26 22:54:11 +0100

    Hook up cao-lang for the webservice

--- web/caoloweb/handler/scripting.py
+++ web/caoloweb/handler/scripting.py
@@ -1,6 +1,8 @@
 from typing import Dict, List, Tuple
-from fastapi import APIRouter, Response, Query, Request
+from fastapi import APIRouter, Response, Query, Request, HTTPException
 import json
+
+import cao_lang
 
 router = APIRouter(prefix="/scripting")
 
@@ -17,3 +19,22 @@ async def get_schema(req: Request):
     # returned data is already json encoded string
     # just write the already encoded response...
     return Response(res_encoded, media_type="application/json")
+
+
+@router.post("/compile")
+async def compile(req: Request):
+    payload: bytes = await req.body()
+    payload = payload.decode(encoding="UTF-8")
+    try:
+        compilation_unit = cao_lang.CompilationUnit.from_json(payload)
+    except ValueError as err:
+        raise HTTPException(
+            status_code=400, detail=f"Failed to parse compilation unit: {err}"
+        )
+
+    try:
+        _program = cao_lang.compile(compilation_unit)
+    except ValueError as err:
+        raise HTTPException(status_code=400, detail=f"Failed to compile program: {err}")
+
+    return {"status": "Ok"}

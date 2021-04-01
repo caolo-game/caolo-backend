c0f766c4fb9ecd877ba015be4528d5911675c4ff
Daniel Kiss <littlesnorrboy@gmail.com>
2021-04-01 19:48:00 +0200

    Enable stdout logging

--- web/start.sh
+++ web/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-gunicorn -w ${WEB_CONCURRENCY:-8} -k uvicorn.workers.UvicornWorker caoloweb.app:app --log-level=info
+gunicorn -w ${WEB_CONCURRENCY:-8} -k uvicorn.workers.UvicornWorker caoloweb.app:app --log-level=info --access-logfile=-
